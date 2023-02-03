ca5a9dbb63af29099a3f978fe5798b199232074b
Ache Miranda Abadin <77676245+achemiranda@users.noreply.github.com>
2023-02-03 23:02:28 +0000

    erase doxygen

--- DOCS/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['index_2ephp_0',['index.php',['../de/d20/index_8php.html',1,'']]]
-];
--- DOCS/html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['photosold_0',['photoSold',['../d3/dcc/md__r_e_a_d_m_e.html',1,'']]]
-];
