b97973a9cd3ce0916392fa276ef9d4e2c91eb3bf
Ache Miranda Abadin <77676245+achemiranda@users.noreply.github.com>
2023-02-05 11:07:08 +0000

    correcion de errores

--- data/config.php
+++ data/config.php
@@ -1,14 +1,28 @@
 <?php
 //conexion a la base de datos creada...
-     return [
-            'db' => [
-                'host' => $_ENV['DB_HOST'],
-                'user' => $_ENV['DB_USER'],
-                'pass' => $_ENV['DB_PASS'],
-                'name' => $_ENV['DB_DB'],
-                'options' => [
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-                ]
-            ]
-        ];
-?>
+return [
+    'db' => [
+        'host' => $_ENV['DB_HOST'],
+        'user' => $_ENV['DB_USER'],
+        'pass' => $_ENV['DB_PASS'],
+        'name' => $_ENV['DB_DB'],
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
+    ]
+];
+
+
+/* conexion a la base de datos creada... */
+/*     return [ */
+/*         'db' => [ */
+/*             'host' => 'localhost', */
+/*             'user' => 'root', */
+/*             'pass' => '', */
+/*             'name' => 'photoSold', */
+/*             'options' => [ */
+/*                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION */
+/*             ] */
+/*         ] */
+/*     ]; */
+/*  */
